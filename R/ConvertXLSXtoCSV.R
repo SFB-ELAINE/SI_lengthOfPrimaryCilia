247c2969f2af85f84762a34d6b7e6350c0a4965a
buddekai <kaibudde@gmail.com>
2022-06-09 13:55:56 +0200

    script to convert xlsx files to csv files

---
+++ R/ConvertXLSXtoCSV.R
@@ -0,0 +1,48 @@
+# Script for converting data from to csv files +++++++++++++++++++++++++++++
+# Author: Kai Budde
+# Created: 2022/03/30
+# Last changed: 2022/06/09
+
+# Delete everything in the environment
+rm(list = ls())
+# close all open plots in RStudio
+graphics.off()
+# Set groundhog day for reproducibility (see https://groundhogr.com)
+groundhog.day <- "2022-03-01"
+
+# install.packages("groundhog")
+library(groundhog)
+pkgs <- c("readxl")
+groundhog.library(pkgs, groundhog.day)
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Please adapt the following parameters ####################################
+
+# Directory with xlsx files
+dir_with_xlsx_files <- "data/manualDetection/190815_AscDexa/originalFiles"
+language <- "en"
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# Convert xlsx to csv files ################################################
+input_files <- list.files(path = dir_with_xlsx_files, pattern = "xlsx", full.names = TRUE)
+output_files <- gsub(pattern = "originalFiles/", replacement = "", x = input_files, ignore.case = TRUE)
+
+
+for(i in 1:length(input_files)){
+  
+  # Read xslx file
+  df_data <- readxl::read_xlsx(path = input_files[i], na = "NA")
+  
+  # Write csv file
+  if(language == "deu"){
+    write.csv2(x = df_data, file = gsub(pattern = "xlsx", replacement = "csv",
+                                        x = output_files[i]), row.names = FALSE)
+  }else{
+    write.csv(x = df_data, file = gsub(pattern = "xlsx", replacement = "csv",
+                                       x = output_files[i]), row.names = FALSE)
+  }
+  
+  
+}
+
