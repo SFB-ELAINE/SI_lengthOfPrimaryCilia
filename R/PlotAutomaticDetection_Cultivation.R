afd026dd27d8d534151e9cfae9f5267279dbeb02
buddekai <kaibudde@gmail.com>
2022-06-17 13:00:01 +0200

    add some statistical calculation

--- R/PlotAutomaticDetection_Cultivation.R
+++ R/PlotAutomaticDetection_Cultivation.R
@@ -21,7 +21,7 @@ if(!any(grepl(pattern = "groundhog", x = installed.packages(), ignore.case = TRU
 
 # Load packages
 library(groundhog)
-pkgs <- c("tidyverse")
+pkgs <- c("tidyverse", "rstatix", "ggpubr")
 groundhog.library(pkgs, groundhog.day)
 
 # Please adapt the following parameters ####################################
@@ -104,7 +104,7 @@ ggsave(filename = paste(output_dir, "all_cilia_total_lengths_violin_plot.png", s
 #TODO: Go through every image and check for bad cilia
 
 df_results_automatic_filtered <- df_results_automatic[!grepl(pattern = "yes", x = df_results_automatic$to_be_removed, ignore.case = TRUE),]
-
+df_results_automatic_filtered <- df_results_automatic_filtered[!is.na(df_results_automatic_filtered$total_length_in_um),]
 
 
 # Total lengths of cilia
@@ -145,4 +145,66 @@ ggsave(filename = paste(output_dir, "all_filtered_cilia_total_lengths_violin_plo
 ggsave(filename = paste(output_dir, "all_filtered_cilia_total_lengths_violin_plot.png", sep="/"),
        width = 297, height = 210, units = "mm")
 
+# Check for normality of data ##############################################
 
+all_normal <- TRUE
+
+for(i in length(names_of_experiments)){
+  # make this example reproducible
+  set.seed(2701)
+  
+  #create data that follows a normal distribution
+  lengths_for_statistical_tests <- df_results_automatic_filtered$total_length_in_um[df_results_automatic_filtered$cultivation == names_of_experiments[i]]
+  
+  #perform shapiro-wilk test
+  test_result <- shapiro.test(lengths_for_statistical_tests)
+  if(test_result$p.value <= 0.05){
+    print(paste("We have a p-value <= 0.05 for the results of ", names_of_experiments[i],".", sep=""))
+    all_normal <- FALSE
+  }
+}
+
+rm(i)
+
+if(all_normal){
+  print("All measurements are normally distributed according to the Shapiro-Wilk test.")
+}
+
+# Compare groups ###########################################################
+
+detection_results <- df_results_automatic_filtered %>%
+  dplyr::group_by(cultivation) %>%
+  rstatix::get_summary_stats(total_length_in_um, type = "mean_sd")
+
+res.aov <- df_results_automatic_filtered %>% anova_test(total_length_in_um ~ cultivation)
+
+
+if(res.aov$`p<.05` == "*"){
+  print("There are significant differences between the groups.")
+  
+  # Pairwise t-tests
+  pairwise_comparison <- df_results_automatic_filtered %>%
+    rstatix::pairwise_t_test(total_length_in_um ~ cultivation, p.adjust.method = "bonferroni") %>% 
+    add_xy_position(x = "cultivation")
+  
+  
+  plot_total_length_violin_t_test <- ggviolin(df_results_automatic_filtered, x = "cultivation", y = "total_length_in_um",
+                                              add = "boxplot", add.params = list(fill = "white")) +
+    stat_pvalue_manual(pairwise_comparison, label = "p.adj", tip.length = 0.01, step.increase = 0.05) +
+    labs(
+      subtitle = get_test_label(res.aov, detailed = TRUE),
+      caption = get_pwc_label(pairwise_comparison)
+    ) +
+    theme_bw(base_size = 18) +
+    theme(#axis.title.y=element_text(size=12),
+      #axis.text.x = element_blank(),
+      axis.ticks.x = element_blank()) +
+    ylab( "Total cilium length in \u03BCm") +
+    xlab("Cultivation")
+  
+  ggsave(filename = paste(output_dir, "all_filtered_cilia_total_lengths_violin_plot_t_test.pdf", sep="/"),
+         width = 297, height = 210, units = "mm")
+  ggsave(filename = paste(output_dir, "all_filtered_cilia_total_lengths_violin_plot_t_test.png", sep="/"),
+         width = 297, height = 210, units = "mm")
+  
+}
