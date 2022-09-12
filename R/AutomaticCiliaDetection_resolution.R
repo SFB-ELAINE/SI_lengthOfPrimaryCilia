b66e79138c6916a68fe3cfe29e38049873c1192f
buddekai <kaibudde@gmail.com>
2022-09-12 13:25:26 +0200

    get and save all information from resolution measurements

--- R/AutomaticCiliaDetection_resolution.R
+++ R/AutomaticCiliaDetection_resolution.R
@@ -1,7 +1,7 @@
 # Script for automatic cilia detection using detectCilia +++++++++++++++++++
 # Author: Kai Budde
 # Created: 2022/07/25
-# Last changed: 2022/07/25
+# Last changed: 2022/09/12
 
 # Delete everything in the environment
 rm(list = ls())
@@ -14,6 +14,9 @@ graphics.off()
 # input_dir <-"C:/Users/Kai/Documents/git/gitHub/detectCilia/tests/movedHere"
 input_dir <- "E:/PhD/Daten/Cilia/resolutionImages"
 output_dir <- "data/automaticDetection/resolution"
+
+# analyzeCziImages TRUE if the czi images have not yet been analyzed
+analyzeCziImages <- FALSE
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
@@ -68,34 +71,107 @@ file_names <- list.files(path = input_dir)
 file_names_czi <- file_names[grepl("czi", file_names)]
 file_names_czi <- paste(input_dir, file_names_czi, sep="/")
 
-# Filter image
-file_names_czi <- file_names_czi[!grepl(pattern = "4096", x = file_names_czi)]
-
 number_of_czi_files <- length(file_names_czi)
 
-# Get and save metadata as csv
-for(i in 1:number_of_czi_files){
-  if(i==1){
-    df_metadata <- readCzi::readCziMetadata(input_file = file_names_czi[i])
-  }else{
-    df_dummy <- readCzi::readCziMetadata(input_file = file_names_czi[i])
-    df_metadata <- rbind(df_metadata, df_dummy)
-    rm(df_dummy)
+if(analyzeCziImages){
+  
+  # Detect cilia
+  for(i in 1:number_of_czi_files){
+    
+    output_list <- detectCilia::detectCilia(input_file_czi = file_names_czi[i])
+    
   }
-}
-rm(i)
-
-write.csv(x = df_metadata,
-          file = paste(output_dir,"/","summary_metadata.csv", sep=""),
-          row.names = FALSE)
-write.csv2(x = df_metadata,
-           file = paste(output_dir,"/","summary_metadata_de.csv", sep=""),
-           row.names = FALSE)
-
-# Detect cilia
-for(i in 1:number_of_czi_files){
+  rm(i)
   
-  output_list <- detectCilia::detectCilia(input_file_czi = file_names_czi[i])
+}else{
+  # Get and save metadata as csv
+  for(i in 1:number_of_czi_files){
+    if(i==1){
+      df_metadata <- readCzi::readCziMetadata(input_file = file_names_czi[i])
+    }else{
+      df_dummy <- readCzi::readCziMetadata(input_file = file_names_czi[i])
+      df_metadata <- rbind(df_metadata, df_dummy)
+      rm(df_dummy)
+    }
+  }
+  rm(i)
   
+  write.csv(x = df_metadata,
+            file = paste(output_dir,"/","summary_metadata.csv", sep=""),
+            row.names = FALSE)
+  write.csv2(x = df_metadata,
+             file = paste(output_dir,"/","summary_metadata_de.csv", sep=""),
+             row.names = FALSE)
+  
+  
+  
+  # Import cilium data from results
+  input_directories <- list.dirs(path = input_dir, full.names = TRUE)
+  input_directories <- input_directories[grepl(pattern = "output", x = input_directories, ignore.case = TRUE)]
+  
+  for(i in 1:length(input_directories)){
+    if(i == 1){
+      df_cilium_summary <- readr::read_csv(file = paste0(input_directories[i], "/cilium_summary.csv"), name_repair = "universal")
+      df_cilium_summary$to_be_removed <- "yes_or_no"
+      
+      df_cilium_summary$fileName <- gsub(pattern = "_output", replacement = ".czi", x = basename(input_directories[i]))
+      df_cilium_summary <- df_cilium_summary %>% 
+        dplyr::relocate(fileName)
+      
+      df_nuclei_number <- readr::read_csv(file = paste0(input_directories[i], "/nuclei_number.csv"), name_repair = "universal")
+      df_nuclei_number$corrected_number <- NA
+      df_nuclei_number$fileName <- gsub(pattern = "_output", replacement = ".czi", x = basename(input_directories[i]))
+      df_nuclei_number <- df_nuclei_number %>% 
+        dplyr::relocate(fileName)
+      
+      df_parameter_list <- readr::read_csv(file = paste0(input_directories[i], "/parameter_list.csv"), name_repair = "universal")
+      df_parameter_list$fileName <- gsub(pattern = "_output", replacement = ".czi", x = basename(input_directories[i]))
+      df_parameter_list <- df_parameter_list %>% 
+        dplyr::relocate(fileName)
+    }else{
+      df_cilium_summary_dummy <- readr::read_csv(file = paste0(input_directories[i], "/cilium_summary.csv"), name_repair = "universal")
+      df_cilium_summary_dummy$fileName <- gsub(pattern = "_output", replacement = ".czi", x = basename(input_directories[i]))
+      df_cilium_summary_dummy <- df_cilium_summary_dummy %>% 
+        dplyr::relocate(fileName)
+      
+      df_nuclei_number_dummy <- readr::read_csv(file = paste0(input_directories[i], "/nuclei_number.csv"), name_repair = "universal")
+      df_nuclei_number_dummy$corrected_number <- NA
+      df_nuclei_number_dummy$fileName <- gsub(pattern = "_output", replacement = ".czi", x = basename(input_directories[i]))
+      df_nuclei_number_dummy <- df_nuclei_number_dummy %>% 
+        dplyr::relocate(fileName)
+      
+      df_parameter_list_dummy <- readr::read_csv(file = paste0(input_directories[i], "/parameter_list.csv"), name_repair = "universal")
+      df_parameter_list_dummy$fileName <- gsub(pattern = "_output", replacement = ".czi", x = basename(input_directories[i]))
+      df_parameter_list_dummy <- df_parameter_list_dummy %>% 
+        dplyr::relocate(fileName)
+      
+      df_cilium_summary <- dplyr::bind_rows(df_cilium_summary, df_cilium_summary_dummy)
+      df_nuclei_number <- dplyr::bind_rows(df_nuclei_number, df_nuclei_number_dummy)
+      df_parameter_list <- dplyr::bind_rows(df_parameter_list, df_parameter_list_dummy)
+    }
+    
+  }
+  rm(list = c("df_cilium_summary_dummy", "df_nuclei_number_dummy", "df_parameter_list_dummy", "i"))
+  
+  write.csv(x = df_cilium_summary,
+            file = paste(output_dir,"/","summary_cilia.csv", sep=""),
+            row.names = FALSE)
+  write.csv2(x = df_cilium_summary,
+             file = paste(output_dir,"/","summary_cilia_de.csv", sep=""),
+             row.names = FALSE)
+  
+  write.csv(x = df_nuclei_number,
+            file = paste(output_dir,"/","summary_nuclei.csv", sep=""),
+            row.names = FALSE)
+  write.csv2(x = df_nuclei_number,
+             file = paste(output_dir,"/","summary_nuclei_de.csv", sep=""),
+             row.names = FALSE)
+  
+  write.csv(x = df_parameter_list,
+            file = paste(output_dir,"/","summary_parameters.csv", sep=""),
+            row.names = FALSE)
+  write.csv2(x = df_parameter_list,
+             file = paste(output_dir,"/","summary_parameters_de.csv", sep=""),
+             row.names = FALSE)
 }
-rm(i)
+
